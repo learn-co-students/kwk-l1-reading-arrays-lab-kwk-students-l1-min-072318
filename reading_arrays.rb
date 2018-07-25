f3ba74fe5416d5d2f9629579e793c411999b71e4
clairespills <purpleprincess372@gmail.com>
2018-07-25 15:30:14 +0000

    Done.

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,41 +1,37 @@
+STUDENT_NAMES=["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
+
 def first_student_by_index
-  student_name=["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
-return student_name [0] 
+return STUDENT_NAMES[0] 
 end
 
 puts first_student_by_index
 
 def fourth_student_by_index
-  student=["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
-  return student [3] 
+  return STUDENT_NAMES[3] 
 end
 
 puts fourth_student_by_index
 
 def last_student_by_index
-  student=["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
-  return student [-1]
+  return STUDENT_NAMES[-1]
 end
 
 puts last_student_by_index
 
 def first_student_by_method
-  student=["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
-  return student.first 
+  return STUDENT_NAMES.first 
 end
 
 puts first_student_by_method
 
 def last_student_by_method
-  student=["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
-  return student.last 
+  return STUDENT_NAMES.last 
 end
 
 puts last_student_by_method
 
 def first_second_and_third_students
-  student=["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
-  return student [0..2] 
+  return STUDENT_NAMES[0..2] 
 end
 
 puts first_second_and_third_students
